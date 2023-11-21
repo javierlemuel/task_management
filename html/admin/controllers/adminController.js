527a10e6ead3b94e1cac587d42777d8d27d01686
javierlemuel <98832519+javierlemuel@users.noreply.github.com>
2023-11-20 20:22:12 -0400

    Add files via upload

--- html/admin/controllers/adminController.js
+++ html/admin/controllers/adminController.js
@@ -4,10 +4,11 @@ const fs = require("fs");
 const user = express();
 user.use(bodyParser.json());
 const path = require('path'); 
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const bcrypt = require('bcrypt');
 const saltRounds = 10; // Number of salt rounds for bcrypt hashing
 module.exports = user;
+const classes = require('../assets/js/classes');
 
 function writeTo(file, data) {
   fs.writeFile(file, JSON.stringify(data), "utf8", (err) => {
@@ -33,9 +34,6 @@ async function comparePassword(plaintextPassword, hash) {
 user.use(express.json());
 
 user.get('/new-route', (req, res) => {
-  //console.log('entered new route');
-  //res.type('html').sendFile(path.join(__dirname, '..', 'test.html'));
-  //console.log(path.join(__dirname, '..', 'admins2.html'));
   res.type('html').sendFile(path.join(__dirname, '..', 'admins2.html'));
 })
 
@@ -61,29 +59,17 @@ user.post('/login', async function(req, res) {
 
       const admin = await usersCollection.findOne({ email });
 
+
+
       if(admin)
       {
           let result = comparePassword(String(password), String(admin['password']));
           if(result)
           {
             console.log("Succesful log in!");
-            res.json({ redirect: '/admin/new-route' });
+            res.json({ redirect: '/admin/new-route', cookie: admin._id.toString()});
           }
       }
-
-      // let admins = data.admins;
-      // let adminExists = Object.values(admins).some(admin => admin.email === email && admin.password === password);
-
-      // if (adminExists) {
-      //   console.log("Succesful log in!");
-      //     console.log('building path');
-
-      //     // Set the content type explicitly to text/html
-      //     console.log('heading to html');
-      //     res.json({ redirect: '/admin/new-route' });
-      //     console.log('after html');
-
-      // } 
       else {
         console.log("Username or password is incorrect");
       }
@@ -113,6 +99,84 @@ user.get('/getAdmins', async (req, res) => {
       await client.close();
   }
 });
+
+user.post('/createAdmin', async function(req, res) {
+
+  let email = req.body.email;
+  let password = req.body.password;
+  email = email.trim();
+  password = password.trim();
+
+  if (!email || !password) {
+      console.log("No email or password provided");
+      res.status(400).json({ error: "Both email and password are required." });
+  } else {
+    const client = new MongoClient('mongodb://0.0.0.0:27017');
+    try{
+
+        await client.connect();
+        const database = client.db('task_management');
+        const usersCollection = database.collection('admins');
+        console.log("DB connect");
+
+        const employee = await usersCollection.findOne({ email });
+
+        if(!employee)
+        {
+          console.log('no existe el email.');
+          const adminToInsert = { "email": email, "password": password, "status":"active"};
+          console.log('data: ', adminToInsert);
+          // Insert the data into the collection
+          const result = await usersCollection.insertOne(adminToInsert);
+          console.log(`Inserted ${result.insertedCount} document(s)`);
+        }
+        else {
+          console.log("Email already exists");
+        }
+    } finally {
+      // Close the MongoDB connection
+      await client.close();
+      console.log('Connection closed');
+    }
+
+  }
+
+})
+
+
+user.post("/getEmployees", async (req, res) => {
+
+      console.log("getEmployees entered");
+      adminID = req.body.adminID;
+      adminID = new ObjectId(adminID);
+      console.log(adminID);
+
+      const client = new MongoClient('mongodb://0.0.0.0:27017');
+
+      await client.connect();
+      const database = client.db('task_management');
+      const usersCollection = database.collection('team');
+      console.log("DB connect");
+
+      // Alternatively, if using a cursor
+      const cursor = await usersCollection.find({ admin: adminID }).toArray();
+      const employeeIDs = cursor.map(doc => doc.employees);
+      const employeesArray = [];
+
+      for (const employeeId of employeeIDs[0]) {
+        //const employeeObjectId = new ObjectId(employeeId);
+        const employeeCollection = database.collection('employee');
+        const employee = await employeeCollection.findOne({ _id: employeeId });
+  
+        // Add the employee details to the array
+        employeesArray.push(employee);
+      }
+
+      //console.log(employeesArray);
+
+      res.json({employees: employeesArray});
+
+})
 
 
 
@@ -166,96 +230,4 @@ user.post('/register', (req, res) => {
   }
 
 })
-
-user.post('/createAdmin', async function(req, res) {
-
-  let email = req.body.email;
-  let password = req.body.password;
-  email = email.trim();
-  password = password.trim();
-
-  if (!email || !password) {
-      console.log("No email or password provided");
-      res.status(400).json({ error: "Both email and password are required." });
-  } else {
-    const client = new MongoClient('mongodb://0.0.0.0:27017');
-    try{
-
-        await client.connect();
-        const database = client.db('task_management');
-        const usersCollection = database.collection('admins');
-        console.log("DB connect");
-
-        const employee = await usersCollection.findOne({ email });
-
-        if(!employee)
-        {
-          console.log('no existe el email.');
-          const adminToInsert = { "email": email, "password": password, "status":"active"};
-          console.log('data: ', adminToInsert);
-          // Insert the data into the collection
-          const result = await usersCollection.insertOne(adminToInsert);
-          console.log(`Inserted ${result.insertedCount} document(s)`);
-        }
-        else {
-          console.log("Email already exists");
-        }
-    } finally {
-      // Close the MongoDB connection
-      await client.close();
-      console.log('Connection closed');
-    }
-
-  }
-
-})
-
-
-// user.post('/register', async (req, res) => {
-
-//   //const client = new MongoClient('mongodb://localhost:27017');
-
-//   try {
-
-//       let email = req.body.email; 
-//       let password = req.body.password;
-
-//       email = email.trim();
-//       password = password.trim();
-
-//       if(email != "" && password != "")
-//       {
-//           // client.connect();
-//           // const database = client.db('test');
-//           // const usersCollection = database.collection('users');
-
-//           // const user = await usersCollection.findOne({ email });
-
-//           if (user) {
-
-//               //if user found, cookie message = username/email already exists
-//               res.send('Email already exists!');
-//               //res.cookie('message', 'Email already exists!');
-//           } else {
-//               //insert query
-//               res.send('Account created!');
-//           }
-//       }
-//       else
-//       {
-//           res.send('Email and password cannot be empty!');
-//           //res.cookie('message', 'Email and password cannot be empty!');
-//       }
-
-//       //res.redirect('/landingPage.html');
-      
-
-//   } catch (err) {
-//       res.send(err);
-//       //console.error(err);
-//    } //finally {
-//       //await client.close();
-//   //}
-  
-// })
 
